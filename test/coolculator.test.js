922014254505d061744cd4661eb8c75d996dbd02
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-14 14:33:13 +0000

    finished

--- test/coolculator.test.js
+++ test/coolculator.test.js
@@ -40,4 +40,54 @@ describe('Coolculator', function() {
     result = mm.double(6)
     expect(result).to.equal(12)
   })
+
+  it('should square', function() {
+    result = mm.square(6)
+    expect(result).to.equal(36)
+  })
+
+  it('should to the power of', function() {
+    result = mm.raise(5, 3)
+    expect(result).to.equal(125)
+  })
+
+  it('should be true if negative', function() {
+    result = mm.isNegative(-54)
+    expect(result).to.equal(true)
+  })
+
+  it('should be true if positive', function() {
+    result = mm.isPositive(-54)
+    expect(result).to.equal(false)
+  })
+
+  it('should return if cool', function() {
+    result = mm.isCool(2452)
+    expect(result).to.equal(true)
+  })
+  
+  it('should sum all', function() {
+    result = mm.sum([2,6,13])
+    expect(result).to.equal(21)
+  })
+
+  it('should multiply all', function() {
+    result = mm.multiplyArray([4,2,1])
+    expect(result).to.equal(8)
+  })
+
+  it('should calculate mean', function() {
+    result = mm.mean([6,4,11])
+    expect(result).to.equal(7)
+  })
+
+  it('should calculte the factorial', function() {
+    result = mm.factorial(6)
+    expect(result).to.equal(720)
+  })
+
+  it('should return a number within a range', function() {
+    result = mm.random(22)
+    expect(result).to.be.at.most(22).and.to.be.at.least(0)
+  })
 })
